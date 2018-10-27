2860305185f8989c818a3077bab76d80acdf6d31
pratik521992 <pratik521992@gmail.com>
2018-10-28 00:52:57 +0530

    Updates

---
+++ precache-manifest.ea38fa3baf2ed7487388043de79e4d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6701e717183d181ab52e",
+    "url": "/Address/static/js/runtime~main.6701e717.js"
+  },
+  {
+    "revision": "31628e889a748eddaf6d",
+    "url": "/Address/static/js/main.31628e88.chunk.js"
+  },
+  {
+    "revision": "e53229eecd8d6cfd9af2",
+    "url": "/Address/static/js/1.e53229ee.chunk.js"
+  },
+  {
+    "revision": "31628e889a748eddaf6d",
+    "url": "/Address/static/css/main.2404c1e0.chunk.css"
+  },
+  {
+    "revision": "4bef4cdb7da26bcc474ea950aa5b23ba",
+    "url": "/Address/index.html"
+  }
+];
